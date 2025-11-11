fe0416be31e1b15e2a34618a7fed1fa205ec153d
Dumba-coded <www.fatimatuzehra@gmail.com>
2025-11-11 10:51:40 +0800

    help me

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost"; //or 128.0.0.1
+    $username = "root";
+    $password = "";
+    $DB = '202511';
+
+    $conn = new mysqli( $servername, $username, $password,$DB );
+
+    if ($conn->connect_error) {
+        die('Connection failed:' .$conn->connect_error);
+    }
+        else{
+    echo "Sucessfully connected.";}
+
